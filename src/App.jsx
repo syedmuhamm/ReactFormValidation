5e6665f9f3fcbbdacc1232127631d4db47695b66
syedmuhammadzulqarnain2@gmail.com <syedmuhammzulqarnain2@gmail.com>
2021-11-20 13:15:46 +0100

    functionlaity of updating form values via object's array of useState() and array of input of forms, added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,93 @@
-import React from "react";
+import FormInput from "./components/FormInput";
+import "./app.css";
+import { useState } from "react";
 
 const App = () => {
+  //since using useState() re-render it with every time a key is entered, we can also use useRef()
+  // making object of type useState to remove repitition of coding lines
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    birthday: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  //an Input Array to remove repitition of Inputs
+  const inputs = [
+    {
+      id: 1,
+      name: "username",
+      type: "text",
+      placeholder: "Username",
+      label: "Username",
+    },
+    {
+      id: 2,
+      name: "email",
+      type: "text",
+      placeholder: "Email",
+      label: "Email",
+    },
+    {
+      id: 3,
+      name: "birthday",
+      type: "text",
+      placeholder: "Birthday",
+      label: "Birthday",
+    },
+    {
+      id: 4,
+      name: "password",
+      type: "password",
+      placeholder: "Password",
+      label: "Password",
+    },
+    {
+      id: 5,
+      name: "confirmPassword",
+      type: "password",
+      placeholder: "Confirm Password",
+      label: "Confirm Password",
+    },
+  ];
+  // we will use <form> and its data
+  // const usernameRef = useRef();
+
+  const handleSubmit = (e) => {
+    //e.preventDefault makes sure that the page does not gets refreshed, every time submit button is clicked.
+    e.preventDefault();
+    // console.log(usernameRef);
+
+    //we can also access data using forms
+
+    // const data = new FormData(e.target);
+    // console.log(Object.fromEntries(data.entries()));
+  };
+
+  const onChange = (e) => {
+    //e.target.name is from inputs array, e.target.value is from live form values
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  console.log(values);
   return (
     <div className="app">
-      <form action=""></form>
+      <form onSubmit={handleSubmit}>
+        {inputs.map((input) => (
+          <FormInput
+            key={input.id}
+            {...input}
+            value={values[input.name]}
+            onChange={onChange}
+          />
+        ))}
+        <button>Submit</button>
+        {/* <FormInput refer={usUsernameeRef} placeholder="Username" /> used for useRef() */}
+        {/* <FormInput name="username" placeholder="Username" />  used to get data from Form directly*/}
+        {/* <FormInput placeholder="Username" setUsername={setUsername} />
+        <FormInput name="email" placeholder="Email" /> since using array on inputs */}
+      </form>
     </div>
   );
 };
--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,10 +1,21 @@
 import "./formInput.css";
 
-const FormInput = () => {
+const FormInput = (props) => {
+  const { label, onChange, id, ...inputProps } = props;
   return (
     <div className="formInput">
-      <label htmlFor="">Username</label>
-      <input type="text" />
+      <label>{label}</label>
+      <input
+        {...inputProps}
+        onChange={onChange}
+
+        // name={props.name} it was used to get data directly via forms
+        // ref={props.refer} it was used to get data using useRef()
+        // placeholder={props.placeholder used for useState() without object of type array for useState()}
+
+        //it was used for useState(), in App.jsx while it was simple without object of type array
+        // onChange={(e) => props.setUsername(e.target.value)}
+      />
     </div>
   );
 };
